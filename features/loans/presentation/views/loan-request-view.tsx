ef3d1f6afbbb4622a7b7e5a214a470aaf6f2edd9
Gerson <gzunta9899@uta.edu.ec>
2025-05-04 17:24:36 -0500

    feat(loans): create base structure and loan-request-view

---
+++ features/loans/presentation/views/loan-request-view.tsx
@@ -0,0 +1,10 @@
+async function LoanRequestView() {
+    return (
+        <div>
+            <h1>Solicitud de Préstamo</h1>
+            {/* TODO: Add NewLoanForm component here */}
+        </div>
+    );
+}
+
+export default LoanRequestView;
