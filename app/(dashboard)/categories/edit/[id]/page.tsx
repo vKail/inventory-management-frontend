772eea990c6d4d028aff783b81aed464346d8626
WADEPAR6 <kparedes9203@uta.edu.ec>
2025-05-26 12:21:21 -0500

    fix#5: set use client in users/page.tsx edit and new

--- app/(dashboard)/categories/edit/[id]/page.tsx
+++ app/(dashboard)/categories/edit/[id]/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import CategoryFormView from "@/features/categories/presentation/views/category-form-view";
 
 export default function CategoryFormEdit({ params }: { params: { id: string } }) {
--- app/(dashboard)/categories/new/page.tsx
+++ app/(dashboard)/categories/new/page.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import CategoryFormView from '@/features/categories/presentation/views/category-form-view';
 
-export default function NewCategoryPage() {
-  return <CategoryFormView params={{}} />;
+export default function NewCategoryPage({ params }: { params: { id: string } }) {
+  return <CategoryFormView id={params.id} />;
 }
