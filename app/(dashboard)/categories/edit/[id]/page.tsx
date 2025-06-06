a3757362e8d4cb24b5c18268979ae4d274d4ad68
WADEPAR6 <kparedes9203@uta.edu.ec>
2025-06-06 13:06:38 -0500

    test10: change the logic with the route

--- app/(dashboard)/categories/edit/[id]/page.tsx
+++ app/(dashboard)/categories/edit/[id]/page.tsx
@@ -1,9 +1,15 @@
-import CategoryFormView from "@/features/categories/presentation/views/category-form-view";
+'use client';
 
-export default function CategoryFormEdit({ params }: { params: { id: string } }) {
+import { useParams } from 'next/navigation';
+import CategoryFormView from '@/features/categories/presentation/views/category-form-view';
+
+export default function CategoryFormEdit() {
+  const params = useParams();
+  const id = params?.id as string;
+
   return (
     <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
-      <CategoryFormView id={params.id} />
+      <CategoryFormView id={id} />
     </div>
   );
 }
