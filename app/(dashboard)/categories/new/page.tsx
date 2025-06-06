b7b58f38fababc984d28493d27c3312083b27cf6
WADEPAR6 <kparedes9203@uta.edu.ec>
2025-06-06 13:28:50 -0500

    test11: change the new apge on categories

--- app/(dashboard)/categories/new/page.tsx
+++ app/(dashboard)/categories/new/page.tsx
@@ -2,6 +2,10 @@
 
 import CategoryFormView from '@/features/categories/presentation/views/category-form-view';
 
-export default function NewCategoryPage({ params }: { params: { id: string } }) {
-  return <CategoryFormView id={params.id} />;
+export default function NewCategoryPage() {
+  return (
+    <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
+      <CategoryFormView id="new" />
+    </div>
+  );
 }
