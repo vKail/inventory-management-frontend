be0075e261918f19e6e8d1a2824bb8256e2db1e2
WADEPAR6 <kparedes9203@uta.edu.ec>
2025-06-06 12:56:58 -0500

    test8: page.tsx of caegories change

--- app/(dashboard)/categories/edit/[id]/page.tsx
+++ app/(dashboard)/categories/edit/[id]/page.tsx
@@ -2,10 +2,16 @@
 
 import CategoryFormView from "@/features/categories/presentation/views/category-form-view";
 
-export default function CategoryFormEdit({
-  params,
-}: {
-  params: { id: string };
-}) {
-  return <CategoryFormView id={params.id.toString()} />;
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default function CategoryFormEdit({ params }: PageProps) {
+  return (
+    <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
+      <CategoryFormView id={params.id} />
+    </div>
+  );
 }
